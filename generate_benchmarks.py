d05a8533c04fd2db0b98c90ebd600e11e1c35bda
emmajingsmith <43729294+emmajingsmith@users.noreply.github.com>
2020-05-30 23:49:16 -0700

    updated to target sim-bpred instead of outorder

--- generate_benchmarks.py
+++ generate_benchmarks.py
@@ -4,11 +4,13 @@ import os
 
 simulator_loc = "/home/emmasmith/simulator/ss3/"
 benchmarks = ["gcc", "perl", "go", "li", "vortex"]
-branch_preds = ["taken", "nottaken", "bimod"]
+branch_preds = ["taken", "nottaken", "bimod", "tage"]
 
 sim_out_order_cmd = '../Run.pl -db ../bench.db -dir ../results/{}_{} -benchmark {} -sim {}sim-outorder -args \"-fastfwd 50000000 -max:inst 10000000  -bpred {}\" > ../results/project/{}_{}.out 2>&1'
+sim_bpred_cmd = '../Run.pl -db ../bench.db -dir ../results/{}_{} -benchmark {} -sim {}sim-bpred -args \"-bpred {} \" > ../results/project/{}_{}.out 2>&1'
 
 for benchmark in benchmarks:
     for branch_pred in branch_preds:
-        os.system(sim_out_order_cmd.format(benchmark, branch_pred, benchmark, simulator_loc, branch_pred, benchmark, branch_pred))
-        print ("Executing: " + sim_out_order_cmd.format(benchmark, branch_pred, benchmark, simulator_loc, branch_pred, benchmark, branch_pred))
+        #os.system(sim_out_order_cmd.format(benchmark, branch_pred, benchmark, simulator_loc, branch_pred, benchmark, branch_pred))
+        os.system(sim_bpred_cmd.format(benchmark, branch_pred, benchmark, simulator_loc, branch_pred, benchmark, branch_pred))
+        print ("Executing: " + sim_bpred_cmd.format(benchmark, branch_pred, benchmark, simulator_loc, branch_pred, benchmark, branch_pred))
